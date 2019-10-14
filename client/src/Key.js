944173e9b385671c25255403cb77eee0b65254d9
ScottE <scotteratigan@gmail.com>
2019-10-14 16:49:40 -0700

    Keys no longer 'stick'

--- client/src/Key.js
+++ client/src/Key.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 export default function Key({ letter, active }) {
-  console.log('letter: "' + letter + '"');
-  return (
-    <div
-      style={{
-        margin: 3,
-        paddingLeft: 5,
-        paddingRight: 5,
-        paddingTop: 3,
-        paddingBottom: 3,
-        border: '1px solid grey',
-        borderRadius: 4,
-        display: 'inline-block',
-        color: active ? 'white' : 'black',
-        backgroundColor: active ? 'blue' : 'white',
-        minWidth: 15,
-        minHeight: '1rem',
-      }}>
-      {letter === ' ' ? 'Spacebar' : letter}
-    </div>
-  );
+  return useMemo(() => {
+    // console.log('letter: "' + letter + '"');
+    return (
+      <div
+        style={{
+          margin: 3,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 3,
+          paddingBottom: 3,
+          border: '1px solid grey',
+          borderRadius: 4,
+          display: 'inline-block',
+          color: active ? 'white' : 'black',
+          backgroundColor: active ? 'blue' : 'white',
+          minWidth: 15,
+          minHeight: '1rem',
+        }}>
+        {letter === ' ' ? 'Spacebar' : letter}
+      </div>
+    );
+  }, [active, letter]);
 }
--- client/src/hooks/useKeyboard.js
+++ client/src/hooks/useKeyboard.js
@@ -4,27 +4,28 @@ import { useEffect, useState } from 'react';
 export default function useKeyboard(setTextPosition) {
   const [activeKeys, setActiveKeys] = useState({});
 
-  function keyDownHandler(event) {
-    // event.location = 1 for left shift and 2 for right shift
-    const { key } = event;
-    // console.log(event.location);
-    if (!activeKeys[key]) {
+  function keyDownHandler(e) {
+    if (!activeKeys[e.key]) {
       setTextPosition(prevPosition => prevPosition + 1);
-      console.log('key is down:', key);
-      const newActiveKeys = { ...activeKeys };
-      newActiveKeys[key] = true;
-      setActiveKeys(newActiveKeys);
+      setActiveKeys(prevActiveKeys => {
+        console.log('key is down:', e.key);
+        const newActiveKeys = { ...prevActiveKeys };
+        newActiveKeys[e.key] = true;
+        return newActiveKeys;
+      });
     }
   }
 
-  function keyUpHandler({ key }) {
-    // console.log('key is up:', key);
-    if (activeKeys[key]) {
-      const newActiveKeys = { ...activeKeys };
-      newActiveKeys[key] = false;
-      setActiveKeys(newActiveKeys);
+  function keyUpHandler(e) {
+    if (activeKeys[e.key]) {
+      setTextPosition(prevPosition => prevPosition + 1);
+      setActiveKeys(prevActiveKeys => {
+        console.log('key is down:', e.key);
+        const newActiveKeys = { ...prevActiveKeys };
+        newActiveKeys[e.key] = false;
+        return newActiveKeys;
+      });
     }
-    // console.log(activeKeys);
   }
 
   useEffect(() => {
